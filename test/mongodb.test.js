6221bddf511c4a157e9d381f4f49d224460e03a0
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-09-26 22:01:53 -0700

    log connection connect stats

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 var funcmatic = require('@funcmatic/funcmatic')
 var MongoDBPlugin = require('../lib/mongodb')
 
-
 describe('Request', () => {
   beforeEach(() => {
     funcmatic.clear()
@@ -31,6 +30,13 @@ describe('Request', () => {
     var plugin = funcmatic.getPlugin('mongodb')
     var event = { }
     var context = { }
+    await funcmatic.invoke(event, context, async (event, context, { mongodb }) => {
+      expect(mongodb).toBeTruthy()
+      expect(await mongodb.stats()).toMatchObject({
+        db: 'test',
+        ok: 1
+      })
+    })
     await funcmatic.invoke(event, context, async (event, context, { mongodb }) => {
       expect(mongodb).toBeTruthy()
       expect(await mongodb.stats()).toMatchObject({
